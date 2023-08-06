adcfc1d863a1dd34042f1d2aaa4b627c9ef40865
Hanna Negash <hnegash.n@gmail.com>
2023-08-05 22:23:56 -0700

    practice for loop and while loop in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// 1. create a new, empty array to hold the messages
+// 2. iterate through the input array and, inside the loop
+// 3. build out the 'thank you' message for each name using string interpolation, then add that message to the new array you created
+// 4. after the loop finishes and all of the messages have been added to the new array, return the new array.
+function writeCards(names, event) {
+  const newNames = [];
+  for (let i = 0; i < names.length; i++) {
+    newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return newNames;
+}
+
+// logs each number when counting down, starting from the number provided
+function countDown() {
+  let number = 10;
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
